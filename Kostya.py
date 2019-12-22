73f4e082c5c2fece44ad8e2c50b5eb8f1e9a0aa8
Kostya <kostya-gabalov@mail.ru>
2019-12-22 13:11:42 +0300

    Two commit

---
+++ Kostya.py
@@ -0,0 +1,12 @@
+
+a = int(input("number 1:"))
+b = int(input("number 1:"))
+try:
+    sum = a + b
+    min = a - b
+    summ = a + b
+except ValueError:
+    print(int(sum))
+    print(min)
+    print(summ)
+
